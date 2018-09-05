ad0fd5fd36a3b537a092425eb0414372980b8489
Nick Geoghegan <nick.geoghegan@airbnb.com>
2018-09-05 12:06:39 +0100

    Adds search4letters

--- 04-chapter/vsearch.py
+++ 04-chapter/vsearch.py
@@ -2,3 +2,8 @@ def search4vowels(phrase:str) -> set:
  """Return any vowels found in a supplied phrase."""
  vowels = set('aeiou')
  return vowels.intersection(set(phrase))
+
+
+def search4letters(phrase:str, letters:str) -> set:
+ """Return a set of the 'letters' found in a supplied 'phrase'."""
+ return set(letters).intersection(set(phrase))
