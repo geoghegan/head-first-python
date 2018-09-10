902f83a0af5c6613cf4fddcdeef18ae3d00c47f3
Nick Geoghegan <nick.geoghegan@airbnb.com>
2018-09-10 11:49:49 +0100

    Updates viewlog to use database

--- 09-chapter/vsearch4web.py
+++ 09-chapter/vsearch4web.py
@@ -42,15 +42,15 @@ def entry_page() -> 'html':
 
 @app.route('/viewlog')
 def view_the_log() -> 'html':
- contents = []
- with open('vsearch.log') as log:
-  for line in log:
-   contents.append([])
-   for item in line.split('|'):
-    contents[-1].append(escape(item))
- titles= ('Form Data', 'Remote_addr', 'User_agent', 'Results')
- return render_template('viewlog.html',
-  the_title='View Log', the_row_titles=titles, the_data=contents,)
+ with UseDatabase(app.config['dbconfig']) as cursor:
+  _SQL = """select phrase, letters, ip, browser_string, results from log"""
+  cursor.execute(_SQL)
+  contents = cursor.fetchall()
+
+  titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+
+  return render_template ('viewlog.html', the_title='ViewLog',
+   the_row_titles=titles, the_data=contents, )
 
 if __name__ == '__main__':
  app.run(debug=True)
