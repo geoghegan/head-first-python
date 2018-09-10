e56ab4701eba8b789fb578da553fcf8ded0e9ce2
Nick Geoghegan <nick.geoghegan@airbnb.com>
2018-09-10 16:15:55 +0100

    Make page1,2,3 check for logins

--- 10-chapter/simple_webapp.py
+++ 10-chapter/simple_webapp.py
@@ -1,5 +1,7 @@
 from flask import Flask, session
 
+from checker import check_logged_in
+
 app = Flask(__name__)
 
 app.secret_key = 'YouWillNeverGuessMySecretKey'
@@ -9,14 +11,17 @@ def hello() -> str:
  return 'Hello from the simple webapp.'
 
 @app.route('/page1')
+@check_logged_in
 def page1() -> str:
  return 'This is page 1.'
 
 @app.route('/page2')
+@check_logged_in
 def page2() -> str:
  return 'This is page 2.'
 
 @app.route('/page3')
+@check_logged_in
 def page3() -> str:
  return 'This is page 3.'
 
