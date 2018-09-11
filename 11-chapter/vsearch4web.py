2a99263fe363a0951e33f39072d4374829756bca
Nick Geoghegan <nick.geoghegan@airbnb.com>
2018-09-11 11:42:59 +0100

    Adds in basic error logging for db connection

--- 11-chapter/vsearch4web.py
+++ 11-chapter/vsearch4web.py
@@ -33,7 +33,10 @@ def do_search() -> 'html':
  letters = request.form['letters']
  title = 'Here are your results: '
  results = str(search4letters(phrase, letters))
- log_request(request, results)
+ try:
+  log_request(request, results)
+ except Exception as err:
+  print('***** Logging failed with this error:', str(err))
  return render_template('results.html', the_phrase=phrase,
   the_letters=letters, the_title=title, the_results=results,)
 
