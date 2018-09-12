f9e7450524da96fd85682d90c12487e7c8aef97e
Nick Geoghegan <nick.geoghegan@airbnb.com>
2018-09-12 11:40:28 +0100

    Formatting

--- 12-chapter/do_convert.py
+++ 12-chapter/do_convert.py
@@ -1,16 +1,15 @@
-
 from datetime import datetime
 import pprint
 
 def convert2ampm(time24: str) -> str:
-    return datetime.strptime(time24, '%H:%M').strftime('%I:%M%p')
+ return datetime.strptime(time24, '%H:%M').strftime('%I:%M%p')
 
 with open('buzzers.csv') as data:
-    ignore = data.readline()
-    flights = {}
-    for line in data:
-        k, v = line.strip().split(',')
-        flights[k] = v
+ ignore = data.readline()
+ flights = {}
+ for line in data:
+  k, v = line.strip().split(',')
+  flights[k] = v
 
 pprint.pprint(flights)
 print()
